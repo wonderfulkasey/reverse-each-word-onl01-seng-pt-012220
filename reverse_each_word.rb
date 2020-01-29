9411c6219a6c5c02b8de71c392e1304a9e1a3f96
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 18:17:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,14 @@
 def reverse_each_word(sentence)
   
   array = sentence.split(" ")
-  #split_sentence = sentence.split(" ")
+  
   new_sentence = []
+  
+  
   array.collect do |sentence|
   new_sentence << sentence_reverse
 end
+
+
  new_sentence.join (" ")
 end
